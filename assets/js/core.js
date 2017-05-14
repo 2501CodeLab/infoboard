4f173a08b4a6d639339fbc2b5fd5df610d4427f7
Zyplos <officialzyplos@gmail.com>
2017-05-14 18:53:38 -0500

    Smart Animations

--- assets/js/core.js
+++ assets/js/core.js
@@ -13,4 +13,24 @@ setTimeout(function() {
 
 function handleAnimEnd(id, dir) {
   console.log(id + " ENDED " + dir);
+  if(dir == "in" && id == "content"){
+    var calcTime = ( $("#headline").text().length + $("#content").text().length ) * 50;
+
+    setTimeout(function() {
+      $("#headline").animateCss("fadeOutLeft", "out");
+    }, calcTime);
+    setTimeout(function() {
+      $("#content").animateCss("fadeOutLeft", "out");
+    }, calcTime + 500);
+
+    //getNewSlide();
+
+    setTimeout(function() {
+      $("#headline").animateCss("fadeInLeft", "in");
+    }, calcTime + 1500);
+    setTimeout(function() {
+      $("#content").animateCss("fadeInLeft", "in");
+    }, calcTime + 2000);
+
+  }
 }
